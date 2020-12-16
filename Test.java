7a57aa9642a1221cf87b0185d9d6fe65b59a8912
Quitumba Culyssander Ferreira <culyssandercentro@gmail.com>
2020-12-16 20:49:17 +0100

    add menu in frontend

--- Test.java
+++ Test.java
@@ -3,4 +3,8 @@ public class Test {
     public static void main(String[]args) {
         System.out.println("Bem-vindo ao challenge backend");
     }
+
+    public static void menu() {
+        System.out.println("Menu do frontend do aplicatico");
+    }
 }
