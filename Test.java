968ac3765be54e5cac77642dfa080e86b3b09eec
Quitumba Culyssander Ferreira <culyssandercentro@gmail.com>
2020-12-16 20:40:42 +0100

    Nova funcionario

--- Test.java
+++ Test.java
@@ -3,4 +3,9 @@ public class Test {
     public static void main(String[]args) {
         System.out.println("Bem-vindo ao challenge backend");
     }
+
+
+    public static void addNovoFuncionario() {
+        System.out.println("Novo funcionario adicionado!");
+    }
 }
